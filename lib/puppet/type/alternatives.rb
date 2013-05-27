234267015638332788f57f44837021a3a35b63e3
Adrien Thebo <git@somethingsinistral.net>
2013-05-26 19:20:54 -0700

    Add Support for `auto` alternative

--- lib/puppet/type/alternatives.rb
+++ lib/puppet/type/alternatives.rb
@@ -1,5 +1,7 @@
 Puppet::Type.newtype(:alternatives) do
 
+  feature :auto, "The alternative has an automatic option"
+
   newparam(:name, :isnamevar => true) do
     desc "The name of the alternative."
   end
@@ -9,6 +11,18 @@ Puppet::Type.newtype(:alternatives) do
 
     validate do |path|
       raise ArgumentError, "path must be a fully qualified path" unless absolute_path? path
+    end
+  end
+
+  newproperty(:auto, :required_features => [:auto]) do
+    desc "Use the automatic option for this alternative"
+
+    newvalue(:true)
+  end
+
+  validate do
+    if self[:auto] and self[:path]
+      raise ArgumentError, "Only one of `auto` and `path` may be set"
     end
   end
 end
